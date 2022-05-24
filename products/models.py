3a7225d5c964f6c594720f887c9d3dcdceb50cea
Jonathan Marshall <jonkylemars@yahoo.com>
2022-05-24 17:40:39 -0500

    Registered Product Model with Admin Site + Completed POST Functionality for Product + Updated REST Framework Decimal Settings

--- products/models.py
+++ products/models.py
@@ -5,6 +5,6 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=3, decimal_places=2)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
     inventory_quantity = models.IntegerField()
     
